f00ad424482b6a33ab60ff3a46bb24693e808350
oromi <exemplumluna+git@gmail.com>
2020-07-09 13:29:15 +0900

    edit config

--- myapp/settings.py
+++ myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '58cll-op!qxa7acsce3d%ray63eoun1zvaw57=z*7*mk$&fo0c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
